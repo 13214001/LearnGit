c1516f919cc929c934873ccb1be9cd22ae55e9fe
intern-os <interns.s2k16@ordersquare.com>
2016-05-31 16:09:14 -0400

    checking push command

--- ArraySum.java
+++ ArraySum.java
@@ -1,10 +1,6 @@
 public class ArraySum{
 public void main(String[] args){
-System.out.println("third change");
-String name="abhishek";
-System.out.println(name);
-System.out.println("yoo");
-System.out.println("second change);
+System.out.println("checking push command");
 }
 
 }
